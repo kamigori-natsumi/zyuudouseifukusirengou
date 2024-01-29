31ba690b2ed17d5ee1c44d8b60a37036a8c7ced3
kamigori-natsumi <152832332+kamigori-natsumi@users.noreply.github.com>
2024-01-29 12:39:04 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,12 +1,3 @@
-
-// window.addEventListener('load', function () {
-//     setTimeout(function(){   
-//     let loadingBg = document.querySelector('.loadingWrapper');
-//     loadingBg.classList.add('activeLoad');
-// },3000);
-// });
-
-
 const faceBani = document.querySelector('.sec01fbanime');
 function faceb(){
     const targetRect = faceBani.getBoundingClientRect();
@@ -34,8 +25,6 @@ function fuwawawa() {
 window.addEventListener('scroll', fuwawawa);
 
 
-
-
 const fadein = document.querySelectorAll('.sec03img');
 function fadeinn() {
     fadein.forEach(function (target) {
@@ -58,13 +47,9 @@ function fadeinn() {
         if (targetRect.top < window.innerHeight && targetRect.bottom >= 0) {
             target.classList.add('fadeinn');
         } 
-        // else {
-        //     target.classList.remove('fadeinn');
-        // }
     });
 };
 window.addEventListener('scroll', fadeinn);
-
 
 
 const bounce = document.querySelectorAll('.sec04title');
@@ -84,28 +69,19 @@ window.addEventListener('scroll', bouncebounce);
 
 function resizeFacebookPlugin() {
     var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
-    // ウィンドウの幅が500px以下の場合
     if (screenWidth <= 500) {
-        // 幅400px、高さ450pxに変更
         document.getElementById('my-fb-page').setAttribute('data-width', '300');
         document.getElementById('my-fb-page').setAttribute('data-height', '300');
     } else {
-        // 元の設定に戻す
         document.getElementById('my-fb-page').setAttribute('data-width', '400');
         document.getElementById('my-fb-page').setAttribute('data-height', '400');
     }
 
-    // Facebookプラグインを再描画
     FB.XFBML.parse();
 }
 
-// ウィンドウのリサイズ時にリサイズ関数を呼び出す
 window.addEventListener('resize', resizeFacebookPlugin);
-
-// ページ読み込み時に初回リサイズを実行
 window.addEventListener('load', resizeFacebookPlugin);
-
 
 
 const swiper = new Swiper('.swiper01', {
@@ -113,27 +89,23 @@ const swiper = new Swiper('.swiper01', {
     effect: 'scroll',
     speed:1500,
 
-    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
     },
 
-    // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
 
-    // And if we need scrollbar
     scrollbar: {
         el: '.swiper-scrollbar',
     },
-    // 自動再生
+
     autoplay: {
         delay: 2000,
     }
 });
-
 
 
 const swiper02 = new Swiper(".swiper02", {
@@ -153,12 +125,6 @@ const swiper02 = new Swiper(".swiper02", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-
-    // allowTouchMove: false,
-    // autoheight: true,
-    // slidesPerView: 1,
-    // centeredSlides: true,
-    // spaceBetween: 5,
 });
 
 
